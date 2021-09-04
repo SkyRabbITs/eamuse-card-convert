efa8ef744ce6d7300c2b17bde0049b2c409b8bf3
SkyRabbITs <36477282+SkyRabbITs@users.noreply.github.com>
2021-09-04 20:00:04 +0900

    Update onefile.cs
    Copied initial line in https://rextester.com/

--- EamuseCardConvert/onefile.cs
+++ EamuseCardConvert/onefile.cs
@@ -1,1 +1,19 @@
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Microsoft (R) Visual C# Compiler version 2.9.0.63208 (958f2354)
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
